e29573e13b97e81325f0e47e7f2c420f0a9d2b13
sachavanruiten <34158557+sachavanruiten@users.noreply.github.com>
2017-12-01 11:40:08 +0100

    Update opdracht2.py

--- opdracht2.py
+++ opdracht2.py
@@ -4,8 +4,10 @@ import numpy as np
 import emcee as emc
 import corner as cor
 
+#Opdracht1
 
 
+# Opdracht2
 def load():
     
     data = np.loadtxt('51Peg mayorqueloz95.dat', dtype = str,unpack=True, skiprows=0)
