9c818984606822aca3404606fcddfb3f1e45cf42
sachavanruiten <34158557+sachavanruiten@users.noreply.github.com>
2017-12-01 12:00:56 +0100

    Update opdracht2.py

--- opdracht2.py
+++ opdracht2.py
@@ -5,7 +5,17 @@ import emcee as emc
 import corner as cor
 
 #Opdracht1
+G = 6.67408*10**(-11) # m^3/kg*s^2
+Mj = 1.898*10**(27) #kg
+Mzon = 1.989*10**(30) #kg
 
+def k(P, i, Mp, Mster):
+    k = ((2*np.pi*G)/(365.25*P))^(1/3) * (Mp*Mj*np.sin(i)) / (Mp*Mj + Mster*Mzon)**(2/3)
+    # P is periode in dagen
+    # Mp is massa v/d planeet in jupitermassa's
+    # i is inclinatiehoek in graden
+    # Mster is massa v/d ster in zonsmassa's
+    return k
 
 # Opdracht2
 def load():
@@ -15,14 +25,3 @@ def load():
     return data
     
 print load()
-
-def plot():
-    
-    data = load()
-    
-    plt.scatter(data[0],data[1])
-    plt.xlabel('date')
-    plt.ylabel('radial velocity')
-    plt.show()
-
-plot()
