e4909de3d4e0021818a7f92b64c07627bbb07383
WKroese <33748385+WKroese@users.noreply.github.com>
2018-04-16 15:38:42 +0200

    Update Data_Reduction.py

--- Data_Reduction.py
+++ Data_Reduction.py
@@ -1,1 +1,35 @@
+#Data reduction code for ALOP 16/04/2018
+#Daphne Abbink, Willem Kroese,Sacha van Ruiten, Yke Rusticus, Kira Strelow
 
+import numpy as np
+import astropy.io as ast
+import matplotlib.pyplot as plt
+
+def import_data():
+    
+    filename = 
+    
+    hdul = ast.open(filename+".fits") 
+    im1 = hdul[1].data 
+    hdr1 = hdul[1].header 
+    hdul.close()
+    
+    
+    return 
+
+
+    
+def select_star(): 
+    #Selecteer ster met DS9
+   
+    return pass
+
+def data_reduction():
+    
+    data = import_data()
+    
+    
+    
+    
+    
+    
