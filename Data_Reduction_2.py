21644594fe1aa0001d616edd4172b984f56c15e3
knstrelow <33930858+knstrelow@users.noreply.github.com>
2018-04-16 15:38:19 +0200

    Update Data_Reduction_2.py

--- Data_Reduction_2.py
+++ Data_Reduction_2.py
@@ -1,1 +1,9 @@
+# 16-04-2018
+# Second file for data reduction code
+# Daphne Abbink, Willem Kroese, Sacha van Ruiten, Yke Rusticus, Kira Strelow
+
+import numpy as np
+import astropy as ast
+import matplotlib.pyplot as plt
+
 
