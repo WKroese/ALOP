526ce5f42b26a92b830640d83363e7d52fd2d90f
knstrelow <33930858+knstrelow@users.noreply.github.com>
2018-04-17 14:26:34 +0200

    Update Data_Reduction.py

--- Data_Reduction.py
+++ Data_Reduction.py
@@ -1,5 +1,6 @@
-#Data reduction code for ALOP 16/04/2018
-#Daphne Abbink, Willem Kroese,Sacha van Ruiten, Yke Rusticus, Kira Strelow
+# Latest update: 17-04-2018
+# Data reduction code for ALOP
+# Daphne Abbink, Willem Kroese, Sacha van Ruiten, Yke Rusticus & Kira Strelow
 
 import numpy as np
 import astropy.io as ast
@@ -7,42 +8,43 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 import sp
 
-##Image = (source + sky)*flat + bias
-#def import_data():
-#    filename = raw_input("Enter filename")
-#    
-#    hdul = ast.open(filename+".fits") 
-#    im1 = hdul[1].data 
-#    hdr1 = hdul[1].header 
-#    hdul.close()
-#    return()
-#
-#path = "Enter path name"
-## Als er meerdere bias opnames zijn: allemaal importeren, bij elkaar optellen en delen door het aantal
-#bias = fits.open(path+"Enter bias file name")
-#Exptimebias = 
-#
-## Per filter doen
-#sky = fits.open(path+"Enter sky file name")
-#Exptimesky =
-#
-#flatR = fits.open(path+"Enter flat R-filter file name")
-#ExptimeflatR =
-#flatV = fits.open(path+"Enter flat V-filter file name")
-#ExptimeflatV =
-#
-#NormflatR = flatR/ExptimeflatR
-#NormflatV = flatV/ExptimeflatV
-#
-## Verkregen data van telescoop - bias
-## Devide by normalised flat
-## Calibrate image
-#
-#Source = (im1 - bias)/normflat - sky# R or V, depending on in which filter the image is
+# Image = (source + sky)*flat + bias
+
+def import_data():
+    filename = raw_input("Enter filename")   
+    hdul = ast.open(filename+".fits") 
+    im1 = hdul[1].data 
+    hdr1 = hdul[1].header 
+    hdul.close()
+    return()
+
+Vfile = raw_input('Enter filename V-exposure\n')
+Rfile = raw_input('Enter filename R-exposure\n')
+Vbias = raw_input('Enter filename V-bias-exposure\n')
+Rbias = raw_input('Enter filename R-bias-exposure\n')
+Vflat = raw_input('Enter filename V-flatfield-exposure\n')
+Rflat = raw_input('Enter filename R-flatfield-exposure\n')
+
+image_V = fits.getdata(Vfile)
+image_R = fits.getdata(Rfile)
+bias_V = fits.getdata(Vbias)
+bias_R = fits.getdata(Rbias)
+flat_V = fits.getdata(Vflat)
+flat_R = fits.getdata(Rflat)
+
+exp_t_image_V = ...
+exp_t_image_R = ...
+exp_t_flat_V = ...
+exp_t_flat_R = ...
 
 
-image_R = fits.getdata("Enter file name Rfilter exposure")
-image_V = fits.getdata("Enter file name Vfilter exposure")
+image_V -= bias_V
+image_R -= bias_R
+normflat_V = flat_V/np.mean(flat_V)
+normflat_R = flat_R/np.mean(flat_R)
+image_V = image_V/(normflat_V/exp_t_flat_V*exp_t_image_V)
+image_R = image_R/(normflat_R/exp_t_flat_R*exp_t_image_R)
+
 
 #hmin is minimal threshold for detection. Should be 3-4 sigma above background RMS
 [xR,yR,fluxR,sharpnessR,roundnessR] = sp.find(image_R,hmin ,5. )
