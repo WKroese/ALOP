46cbbde3503ea6aa967fec0dfa8356733c9676af
knstrelow <33930858+knstrelow@users.noreply.github.com>
2018-04-17 13:59:50 +0200

    Update Data_Reduction_2.py

--- Data_Reduction_2.py
+++ Data_Reduction_2.py
@@ -1,4 +1,4 @@
-# 16-04-2018
+# Latest update: 17-04-2018
 # Second file for data reduction code
 # Daphne Abbink, Willem Kroese, Sacha van Ruiten, Yke Rusticus, Kira Strelow
 
@@ -24,13 +24,16 @@ R_m = -2.5 * np.log10(R_Flux / R_Flux[...]) + R_m_ref
 # X_Flux[...] is the target and the other one is the array of reference stars
 err_V_m = np.sqrt((2.5/(V_Flux[...]*np.log(10))*err_V_Flux[...])**2 + ((2.5/(V_Flux[...]*np.log(10))*err_V_Flux[...])**2 + err_V_m_ref**2)
 err_R_m = np.sqrt((2.5/(R_Flux[...]*np.log(10))*err_R_Flux[...])**2 + ((2.5/(R_Flux[...]*np.log(10))*err_R_Flux[...])**2 + err_R_m_ref**2)
-# .... gewogen gemiddelde
+
+V = np.sum(V_m/(err_V_m)**2)
+R = np.sum(R_m/(err_R_m)**2)
+err_V = np.sum(1./(err_V_m)**2)
+err_R = np.sum(1./(err_R_m)**2)
                   
 # We can use this to add new lines to an existing text file (adding new data points)
-#with open('file.txt', 'a') as file:
-#    file.writelines(V_m, '  ', err_V_m, '  ', R_m, '  ', err_R_m)
+#with open('test.txt', 'a') as file:
+#    file.writelines('{}\t{}\t{}\t{}\t{}\n'.format(t, V, err_V, R, err_R))
 
-# This will become code to read in all saved data points and plot them.         
-#with open("tab-separated-values") as tsv:
-#    for line in csv.reader(tsv, dialect="excel-tab"): #You can also use delimiter="\t" rather than giving a dialect.
-#        ... 
+# This will become code to read in all saved data points and plot them.
+#t, V_m, err_V_m, R_m, err_R_m = np.loadtxt('test.txt', delimiter='\t', unpack=True)
+                  
