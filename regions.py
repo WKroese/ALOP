14b94fc6c0ba30e5c78f03acb487bc23ddb7a040
knstrelow <33930858+knstrelow@users.noreply.github.com>
2018-04-28 02:42:32 +0200

    Update regions.py

--- regions.py
+++ regions.py
@@ -2,3 +2,14 @@ import numpy as np
 import astropy.io as ast
 from astropy.io import fits
 import matplotlib.pyplot as plt
+
+np.loadtxt
+
+
+def open_file():
+    for filename in filenames:
+        hdul = fits.open(filename) 
+        im = hdul[4].data 
+        hdr0 = hdul[0].header 
+        hdul.close()
+        return im
